1c487220646aced536aad3b35aa8628f66123007
ritik ghodke <ritikghodke70@gmail.com>
2026-04-03 03:01:07 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,3 @@
-
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-  
-//     <App />
-  
-// )
-
-
-
-
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
