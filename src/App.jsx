b89eb0328bbd464125a524ab982b9c93c5fc3ffd
ritik ghodke <ritikghodke70@gmail.com>
2026-04-13 12:46:18 +0530

    fix: addstory import

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import EditPassword from './Components/EditPassword'
 import ViewProfile from './Components/ViewProfile'
 import Requests from './Components/Requests'
 import Notifications from './Components/Notification'
-import AddStory from './Components/AddStory'
+import AddStory from './Components/Addstory'
 
 export const uiContext = createContext()
 
